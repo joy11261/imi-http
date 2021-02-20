343df97f7b9e404de85f824a7cc42f772a87b8ee
wangsai <wangsai@yuwantech.com>
2021-02-20 13:42:10 +0800

    测试控制器

--- ApiServer/Controller/TestController.php
+++ ApiServer/Controller/TestController.php
@@ -25,4 +25,55 @@ class TestController extends HttpController
             'hello' =>  'test11',
         ];
     }
+
+    /**
+     * @Action
+     * @Route(url="/article/{id}")
+     *
+     * @return void
+     */
+    public function article($id){
+
+        return [
+            'code' => 0,
+            'id'   => $id,
+        ];
+    }
+
+    /**
+     * @Action
+     * @Route(url="/article/delete/",method="POST")
+     *
+     * @return void
+     */
+    public function deleteArticle() {
+        return ['data' => 111];
+    }
+
+    /**
+     * @Action
+     * @Route (url="/login",domain="127.0.0.1:8080")
+     *
+     * @return void
+     */
+    public function login() {
+        return ['data' => 'login'];
+    }
+
+
+
+    /**
+     * @Action
+     * @Route(url="/testGet/{name}")
+     *
+     * @return void
+     */
+    public function testGet($name)
+    {
+        return [
+            'name'  =>  $name,
+        ];
+    }
+
+
 }
